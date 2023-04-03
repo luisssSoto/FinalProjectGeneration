7123bbfb03847756bed29369534d668663b4dad1
ElsyG <e.gonzalezcen@gmail.com>
2023-04-03 10:13:40 -0600

    actualización home

---
+++ FinalProject/FinalProject/YukArt/yukArtMySQL/YukArt DataBase/YukArt/src/main/java/com/labanda/yukart/YukArtApplication.java
@@ -0,0 +1,13 @@
+package com.labanda.yukart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YukArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YukArtApplication.class, args);
+	}
+
+}
